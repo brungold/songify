b864ee118b161281ce3b454c2fc01f837944b1f2
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-08-28 15:12:35 +0200

    added class SongController

---
+++ src/main/java/com/songify/SongsController.java
@@ -0,0 +1,13 @@
+package com.songify;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SongsController {
+
+    @GetMapping("/songs")
+    public void f(){
+
+    }
+}
