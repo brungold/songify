3f8ec33ade374dc7a8ddae5049c8055aa9b86e36
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-08-28 18:35:19 +0200

    added Header option to getSongById

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -1,17 +1,16 @@
 package com.songify;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@Log4j2
 public class SongsController {
     Map<Integer, String> database = new HashMap<>();
     //GET /songs + GET query Param /songs?id=100
@@ -36,7 +35,8 @@ public class SongsController {
     }
     //GET /songs?id=100
     @GetMapping("/songs/{id}")
-    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id){
+    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id, @RequestHeader(required = false) String requestId){
+        log.info(requestId);
         String song = database.get(id);
         if(song == null){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
