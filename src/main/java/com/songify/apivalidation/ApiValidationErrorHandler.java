05d94fe80639c281c3b75aee4a1491879e843a70
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-08-28 23:01:18 +0200

    added annotations tp ApiValidationErrorHandler method @ResponseBody and @ResponseStatus(HttpStatus.BAD_REQUEST)

--- src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
+++ src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
@@ -1,14 +1,19 @@
 package com.songify.apivalidation;
 
 import com.songify.song.SongsRestController;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 // ContollerAdvice wyższy poziom obsługiwania błędu
 @ControllerAdvice(assignableTypes = SongsRestController.class)
 public class ApiValidationErrorHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiValidationErrorResponseDto handleValidationException(MethodArgumentNotValidException exception){
         String message = exception.getMessage();
         return new ApiValidationErrorResponseDto("handled exception");
