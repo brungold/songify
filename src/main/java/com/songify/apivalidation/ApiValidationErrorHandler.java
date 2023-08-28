412872e37f7cb0b92af00db98cd6eacff244de07
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-08-28 22:52:57 +0200

    filled in class ApiValidationErrorHandler method handleValidationException with code

--- src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
+++ src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
@@ -1,4 +1,15 @@
 package com.songify.apivalidation;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+// ContollerAdvice wyższy poziom obsługiwania błędu
+@ControllerAdvice
 public class ApiValidationErrorHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleValidationException(MethodArgumentNotValidException exception){
+        String message = exception.getMessage();
+        return "handled exception";
+    }
 }
