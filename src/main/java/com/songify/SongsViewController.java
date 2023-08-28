8fe75503a0efe3df84273e1b162e9d6ea5c9f17b
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-08-28 19:11:05 +0200

    adding Model to SongsViewController to method for html -> view songs

--- src/main/java/com/songify/SongsViewController.java
+++ src/main/java/com/songify/SongsViewController.java
@@ -2,15 +2,24 @@ package com.songify;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class SongsViewController {
+    private Map<Integer, String> database = new HashMap<>();
     @GetMapping("/")
     public String home(){
         return "home";
     }
     @GetMapping("/view/songs")
-    public String songs(){
+    public String songs(Medel model){
+        database.put(1, "shawnmendes song1");
+        database.put(2, "metallica one");
+        database.put(3, "sting two");
+        database.put(4, "barnaba three");
+
         return "songs";
     }
 }
