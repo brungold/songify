3de35492c8a2be29d25d47615c67b0e8d35df7bd
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-08-28 23:05:58 +0200

    added a very important method that pulls out a list of all the errors the client has made so far

--- src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
+++ src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
@@ -1,12 +1,16 @@
 package com.songify.apivalidation;
 
 import com.songify.song.SongsRestController;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 // ContollerAdvice wyższy poziom obsługiwania błędu
 @ControllerAdvice(assignableTypes = SongsRestController.class)
@@ -15,7 +19,14 @@ public class ApiValidationErrorHandler {
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiValidationErrorResponseDto handleValidationException(MethodArgumentNotValidException exception){
-        String message = exception.getMessage();
+        List<String> messages = getErrorFromException(exception);
         return new ApiValidationErrorResponseDto("handled exception");
+    }
+    private List<String> getErrorFromException(MethodArgumentNotValidException exception){
+        return exception.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
     }
 }
