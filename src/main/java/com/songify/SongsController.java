3b2885cce6adb1051673e481d20efb7f2773b53e
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-08-28 19:25:41 +0200

    refactored 'database' to creating at the begin not at the method

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -12,14 +12,16 @@ import java.util.stream.Collectors;
 @RestController
 @Log4j2
 public class SongsController {
-    Map<Integer, String> database = new HashMap<>();
+    Map<Integer, String> database = new HashMap<>(Map.of(
+            1, "shawnmendes song1",
+        2, "metallica one",
+        3, "sting two",
+        4, "barnaba three",
+    ));
     //GET /songs + GET query Param /songs?id=100
     @GetMapping("/songs")
     public ResponseEntity<SongResponseDto> getAllSongs(@RequestParam(required = false) Integer limit){
-        database.put(1, "shawnmendes song1");
-        database.put(2, "metallica one");
-        database.put(3, "sting two");
-        database.put(4, "barnaba three");
+
         if(limit != null){
             Map<Integer, String> limitedMap = database.entrySet()
                     .stream()
