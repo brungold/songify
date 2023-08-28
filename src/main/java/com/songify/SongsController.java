7215a6f3f9e7d6f10f0f4fd41e8b05fa1126a71c
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-08-28 15:34:12 +0200

    added second GetMapping method -> getSongById. @PathVariable by id

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -2,16 +2,30 @@ package com.songify;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class SongsController {
+    Map<Integer, String> database = new HashMap<>();
 
     @GetMapping("/songs")
     public ResponseEntity<SongResponseDto> getAllSongs(){
-        SongResponseDto response = new SongResponseDto(List.of("shawnmendes song1", "metallica song2"));
+        database.put(1, "shawnmendes song1");
+        database.put(2, "metallica one");
+        SongResponseDto response = new SongResponseDto(database);
+        return ResponseEntity.ok(response);
+
+    }
+
+    @GetMapping("/songs/{id}")
+    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable String id){
+        String song = database.get(id);
+        SingleSongResponseDto response = new SingleSongResponseDto(song);
         return ResponseEntity.ok(response);
 
     }
