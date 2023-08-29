a06580fc7671149d86bb2b2dd8663ab5cd7ded9e
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-08-29 14:30:25 +0200

    added important comments about RunTimeException scheme

--- src/main/java/com/songify/song/controller/SongNotFoundException.java
+++ src/main/java/com/songify/song/controller/SongNotFoundException.java
@@ -1,6 +1,9 @@
 package com.songify.song.controller;
 
 public class SongNotFoundException extends RuntimeException {
+    //RuntimeException klasa nadrzedna. Runtime jest zbyt odgólne
+    //Poniższa nie jest sama w sobie runtimeexceptionem. mamy pewność że piosenka nie została odnaleziona
     public SongNotFoundException(String message) {
+        super(message);// super -> wywołuje konstruktor klasy która jest dziedziczona
     }
 }
