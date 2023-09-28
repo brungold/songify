6999b3f72a88163964f0a2e539623590f953595a
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-09-28 18:10:49 +0200

    added @Transactional annotation to SongAdder and SongDeleter

--- src/main/java/com/songifyDatabase/song/domain/service/SongAdder.java
+++ src/main/java/com/songifyDatabase/song/domain/service/SongAdder.java
@@ -2,11 +2,13 @@ package com.songifyDatabase.song.domain.service;
 
 import com.songifyDatabase.song.domain.model.Song;
 import com.songifyDatabase.song.domain.repository.SongRepository;
+import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
+@Transactional
 public class SongAdder {
     private final SongRepository songRepository;
 
--- src/main/java/com/songifyDatabase/song/domain/service/SongDeleter.java
+++ src/main/java/com/songifyDatabase/song/domain/service/SongDeleter.java
@@ -1,11 +1,13 @@
 package com.songifyDatabase.song.domain.service;
 
 import com.songifyDatabase.song.domain.repository.SongRepository;
+import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
+@Transactional
 public class SongDeleter {
     private final SongRepository songRepository;
     private final SongRetriever songRetriever;
