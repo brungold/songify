982ce243e40d8c364f27a058b1d5fb044bd083ee
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2024-07-28 22:56:00 +0200

    added migration, flyway, refactored Song class, added SongLanguage enum and created BaseEntity superclass for Song

---
+++ src/main/java/com/songifyDatabase/song/domain/model/SongLanguage.java
@@ -0,0 +1,12 @@
+package com.songifyDatabase.song.domain.model;
+
+public enum SongLanguage {
+    ENGLISH,
+    SPANISH,
+    POLISH,
+    FRENCH,
+    GERMAN,
+    ITALIAN,
+    JAPANESE,
+    OTHER
+}
