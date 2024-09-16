df2de50bfa0f95904fd50400fd82b4e27d8badcc
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2024-09-16 18:25:08 +0200

    unit test // added TC: added draft mockito test

--- src/main/java/com/songifyDatabase/domain/songplayer/SongPlayerFacade.java
+++ src/main/java/com/songifyDatabase/domain/songplayer/SongPlayerFacade.java
@@ -1,4 +1,26 @@
 package com.songifyDatabase.domain.songplayer;
 
+import com.songifyDatabase.domain.crud.SongifyCrudFacade;
+import com.songifyDatabase.domain.crud.dto.SongDto;
+
 class SongPlayerFacade {
+
+    private final SongifyCrudFacade songifyCrudFacade;
+    private final YoutubeHttpClient youtubeHttpClient;
+
+
+    public SongPlayerFacade(SongifyCrudFacade songifyCrudFacade, YoutubeHttpClient youtubeHttpClient) {
+        this.songifyCrudFacade = songifyCrudFacade;
+        this.youtubeHttpClient = youtubeHttpClient;
+    }
+
+    public String playSongWithId(Long id){
+        SongDto songDtoById = songifyCrudFacade.findSongDtoById(id);
+        String name = songDtoById.name();
+        String result = youtubeHttpClient.playSongByName(name);
+        if(result.equals("success")){
+            return result;
+        }
+        throw new RuntimeException("some error - result failed");
+    }
 }
---
+++ src/main/java/com/songifyDatabase/domain/songplayer/YoutubeHttpClient.java
@@ -0,0 +1,8 @@
+package com.songifyDatabase.domain.songplayer;
+
+interface YoutubeHttpClient {
+
+
+    String playSongByName(String name);
+}
+
