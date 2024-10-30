c15dd2e45ee1344ca66a88c85d4805e49163f708
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2024-10-30 21:58:54 +0100

    added SecurityFilterChain  method in the SecurityConfig + only admin can mutate data

--- src/main/java/com/songifyDatabase/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songifyDatabase/infrastructure/security/SecurityConfig.java
@@ -3,9 +3,13 @@ package com.songifyDatabase.infrastructure.security;
 import com.songifyDatabase.domain.usercrud.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -16,8 +20,40 @@ public class SecurityConfig {
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
 //        return NoOpPasswordEncoder.getInstance();
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(c -> c.disable());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/swagger-resources/**").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("/users/register/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/songs/**").permitAll()
+//                .requestMatchers(HttpMethod.POST, "/token/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/artist/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/albums/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/artists/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/genres/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, "/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/artists/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, "/artists/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/artists/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/artists/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/albums/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/albums/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/genres/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/users/**").hasRole("ADMIN")
+                .anyRequest().authenticated());
+        return http.build();
     }
 }
