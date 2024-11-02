7634169a3502c87589bd4bc88c2653c58f500269
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2024-11-02 15:23:00 +0100

    added ClockConfig class
    added JwtConfigurationProperties and JwtTokenGenerator fileds at JWT.create
    
    added properties

--- src/main/java/com/songifyDatabase/SongifyApplication.java
+++ src/main/java/com/songifyDatabase/SongifyApplication.java
@@ -1,9 +1,12 @@
 package com.songifyDatabase;
 
+import com.songifyDatabase.infrastructure.security.jwt.JwtConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {JwtConfigurationProperties.class})
 public class SongifyApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/songifyDatabase/infrastructure/security/jwt/JwtTokenGenerator.java
+++ src/main/java/com/songifyDatabase/infrastructure/security/jwt/JwtTokenGenerator.java
@@ -1,21 +1,40 @@
 package com.songifyDatabase.infrastructure.security.jwt;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.songifyDatabase.infrastructure.security.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.time.*;
+
 @Component
 @RequiredArgsConstructor
 class JwtTokenGenerator {
 
+    public static final String ROLES_CLAIM_NAME = "roles";
+
     private final AuthenticationManager authenticationManager;
+    private final Clock clock;
+    private final JwtConfigurationProperties properties;
 
     public String authenticateAndGenerateToken(String username, String password) {
         UsernamePasswordAuthenticationToken authenticate = new UsernamePasswordAuthenticationToken(username, password);
         Authentication authentication = authenticationManager.authenticate(authenticate);
-        return JWT.create().sign();
+        SecurityUser securityUser = (SecurityUser) authentication.getPrincipal();
+        Instant issuedAt = LocalDateTime.now(clock).toInstant(ZoneOffset.UTC);
+//        Instant issuedAt = LocalDateTime.now(clock).toInstant(ZoneOffset.of("Europe/Warsaw"));
+        Instant expiresAt = issuedAt.plus(Duration.ofMinutes(properties.expirationMinutes()));
+        Algorithm algorithm = Algorithm.HMAC256(properties.secret());
+        return JWT.create()
+                .withSubject(securityUser.getUsername())
+                .withIssuedAt(issuedAt)
+                .withExpiresAt(expiresAt)
+                .withIssuer(properties.issuer())
+                .withClaim(ROLES_CLAIM_NAME, securityUser.getAuthritiesAsString())
+                .sign(algorithm);
     }
 }
