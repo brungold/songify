2e7e9cd9c2c7503bb6e0b3e7ab4049955e9f51c7
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2024-11-02 15:56:15 +0100

    added JwtKeyPairConfig - asymmetric key

--- src/main/java/com/songifyDatabase/infrastructure/security/jwt/JwtTokenController.java
+++ src/main/java/com/songifyDatabase/infrastructure/security/jwt/JwtTokenController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.NoSuchAlgorithmException;
+
 @RestController
 @RequiredArgsConstructor
 public class JwtTokenController {
@@ -13,7 +15,7 @@ public class JwtTokenController {
     private final JwtTokenGenerator tokenGenerator;
 
     @PostMapping("/token")
-    public ResponseEntity<JetResponseDto> authenticateAndGenerateToken(@RequestBody TokenRequestDto dto){
+    public ResponseEntity<JetResponseDto> authenticateAndGenerateToken(@RequestBody TokenRequestDto dto) throws NoSuchAlgorithmException {
         //1. generujemy token
         String token = tokenGenerator.authenticateAndGenerateToken(dto.username(), dto.password());
         return ResponseEntity.ok(
